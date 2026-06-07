e6f18fa5a93073dca68684257a793c33f0ab7f4b
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 14:10:32 +0300

    Update ViewEventDetails.jsx

--- myreactapp/src/components/ViewEventDetails.jsx
+++ myreactapp/src/components/ViewEventDetails.jsx
@@ -77,7 +77,7 @@ navigate(`/admindashboard/${useremail}/Events`);
         
         
             try {
-              const res = await fetch('http://localhost:4000/graphql', {
+              const res = await fetch('https://local-community-dashboard.vercel.app', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json',...authHeader },
                 body: JSON.stringify(graphqlQuery),
