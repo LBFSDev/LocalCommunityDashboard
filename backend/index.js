3ce6e4fc8d62c374afb6ec45465cca598cbd0a6f
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 16:09:56 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -827,8 +827,9 @@ const token = jwt.sign(
     // If email not found in either table
     return { role: null, message: "User not found",token:null };
   }catch(err){
+      console.log(err);
     throw new Error("something is wrong!");
-    console.log(err);
+    // console.log(err);
   }
   },
 
