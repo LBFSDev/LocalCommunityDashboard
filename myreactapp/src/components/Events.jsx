90dd2e40bb8ae25c4cf76029f7571e555ed27cb4
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 14:00:19 +0300

    Update Events.jsx

--- myreactapp/src/components/Events.jsx
+++ myreactapp/src/components/Events.jsx
@@ -82,7 +82,7 @@ const [Error, setError] = useState([]);
     
     
         try {
-          const res = await fetch('http://localhost:4000/graphql', {
+          const res = await fetch('https://local-community-dashboard.vercel.app', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' ,...authHeader},
             body: JSON.stringify(graphqlQuery),
