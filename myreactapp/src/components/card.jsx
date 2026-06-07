6561bfec4547f80bc4b4e9610195ed1490cedea8
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 14:13:33 +0300

    Update card.jsx

--- myreactapp/src/components/card.jsx
+++ myreactapp/src/components/card.jsx
@@ -36,7 +36,7 @@ const navigate = useNavigate();
   
   
       try {
-        const res = await fetch('http://localhost:4000/graphql', {
+        const res = await fetch('https://local-community-dashboard.vercel.app', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(graphqlQuery),
