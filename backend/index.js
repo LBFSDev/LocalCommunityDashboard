daa7c137b90435d04e6910ac923df256c5e23b3e
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 17:08:13 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -253,7 +253,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 
 
