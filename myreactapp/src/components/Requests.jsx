8c4355aca157645526a496ab1354d57bf830ecf5
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 14:07:22 +0300

    Update Requests.jsx

--- myreactapp/src/components/Requests.jsx
+++ myreactapp/src/components/Requests.jsx
@@ -98,7 +98,7 @@ const authHeader = token && token !== "undefined"
 
 
     try {
-      const res = await fetch('http://localhost:4000/graphql', {
+      const res = await fetch('https://local-community-dashboard.vercel.app', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json',...authHeader },
         body: JSON.stringify(graphqlQuery),
@@ -142,7 +142,7 @@ async function updatestatus(id , status){
 
 
     try {
-      const res = await fetch('http://localhost:4000/graphql', {
+      const res = await fetch('https://local-community-dashboard.vercel.app', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json',...authHeader },
         body: JSON.stringify(graphqlQuery),
