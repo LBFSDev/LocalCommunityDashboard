fc3d6c3b76dbbd119e5526e5e45032363273996c
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 14:53:38 +0300

    Update NewContact.jsx

--- myreactapp/src/components/NewContact.jsx
+++ myreactapp/src/components/NewContact.jsx
@@ -101,7 +101,7 @@ async function createcontact(e){
   
   
       try {
-        const res = await fetch('https://local-community-dashboard.vercel.app', {
+        const res = await fetch('https://localcommunitydashboard.onrender.com/graphql', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json',...authHeader },
           body: JSON.stringify(graphqlQuery),
