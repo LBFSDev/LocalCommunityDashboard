c9c76eaefd48096a5cc1070732d741254b50783f
LBFSDev <abbasyazbek534@gmail.com>
2026-06-07 13:57:40 +0300

    Update CardPro.jsx

--- myreactapp/src/components/CardPro.jsx
+++ myreactapp/src/components/CardPro.jsx
@@ -85,7 +85,7 @@ const navigate = useNavigate();
   
   
       try {
-        const res = await fetch('http://localhost:4000/graphql', {
+        const res = await fetch('https://local-community-dashboard.vercel.app', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json',...authHeader  },
           body: JSON.stringify(graphqlQuery),
